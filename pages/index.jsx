259a1095f41435141f63728e32ff37c766c93d36
BrieanneH <howard.brieanne@gmail.com>
2020-09-08 02:36:37 -0500

    updates to wHOLEsome repo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
-
+import { CLIENT_NAME } from 'lib/constants';
 
 const Index = () => {
-    return (
-        <div className=" text-custom text-black block mx-auto">
-            sdfsdaf
-        </div>
-    )
-}
+	return (
+		<Fragment>
+			<div className=' text-custom text-black block mx-auto'>
+				Welcome to {CLIENT_NAME}&apos;s portfolio
+			</div>
+		</Fragment>
+	);
+};
 
 export default Index;
